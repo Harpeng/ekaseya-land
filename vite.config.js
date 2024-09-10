14cfbdac4d895a63dad06269b517993d54657a3f
Harpeng <gondarev75@gmail.com>
2024-09-10 21:39:33 +0300

    setting vite-config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: `assets/[name].[hash:8].${new Date().getTime()}.[ext]`,
+        chunkFileNames: `assets/[name].[hash:8].${new Date().getTime()}.js`,
+        entryFileNames: `assets/[name].[hash:8].${new Date().getTime()}.js`,
+      }
+    }
+  },
   plugins: [
     vue(),
   ],
